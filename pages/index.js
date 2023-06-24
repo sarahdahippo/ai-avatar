6783751fc9552987ec6398a42ae23ac661639282
sarahdahippo <sarahcheah22@gmail.com>
2023-06-24 23:25:11 +0800

    api + frontend

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,148 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  // don't retry more than 20 times
+  const maxRetries = 20;
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState('');
+
+  // numbers of retries
+  const [retry, setRetry] = useState(0);
+
+  // number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState('');
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const generateAction = async () => {
+    console.log('Generating...');
+
+    // make sure there is no double click
+    if (isGenerating && retry === 0) return;
+    setIsGenerating(true);
+
+    // if this is a retry request, take away retryCount
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
+
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+  
+    const data = await response.json();
+
+    // if model still loading
+    if (response.status === 503) {
+      setRetry(data.estimated_time);
+      return;
+    }
+  
+    // If another error, drop error
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      return;
+    }
+    
+    setFinalPrompt(input);
+
+    // remove content from input box
+    setInput('');
+    setImg(data.image);
+    setIsGenerating(false);
+  }
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+  
+  // make retries until we receive image response
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+        setRetryCount(maxRetries);
+        return;
+        }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI Avatar Generator</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>AI Avatar Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Turn me into anyone you want! Make sure you refer to me as "sarah" in the prompt.</h2>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className="prompt-buttons">
+              {/* Tweak classNames to change classes */}
+              <a
+                className={
+                  isGenerating ? 'generate-button loading' : 'generate-button'
+                }
+                onClick={generateAction}
+              >
+                {/* Tweak to show a loading indicator */}
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+            </div>
           </div>
         </div>
+        {/* Add output container */}
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={finalPrompt} />
+            {/* Add prompt here */}
+            <p>{finalPrompt}</p>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
